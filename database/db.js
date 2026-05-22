1f193ab9c925248e5332abfe91c42a2fe1936add
Muhammad Faizan <muhammadfaizan45890@gmail.com>
2026-05-22 16:47:57 +0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,30 +1,22 @@
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     const uri = process.env.MONGODB_URI;
-
-//     if (!uri) {
-//       console.log("❌ MONGODB_URI is missing in Railway env variables");
-//       return;
-//     }
-
-//     await mongoose.connect(uri);
-
-//     console.log("✅ MongoDB connected successfully");
-//   } catch (err) {
-//     console.log("❌ MongoDB connection failed:");
-//     console.log(err.message);
-//   }
-// };
-
-// export default connectDB;
-
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  console.log("🔥 MONGODB_URI =", process.env.MONGODB_URI);
+  try {
+    const uri = process.env.MONGODB_URI;
+
+    if (!uri) {
+      console.log("❌ MONGODB_URI is missing in Railway env variables");
+      return;
+    }
+
+    await mongoose.connect(uri);
+
+    console.log("✅ MongoDB connected successfully");
+  } catch (err) {
+    console.log("❌ MongoDB connection failed:");
+    console.log(err.message);
+  }
 };
 
 export default connectDB;
+

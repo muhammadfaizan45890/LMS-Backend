211092b853a38fa09cfce9bb6c0ab7803a5b9722
Muhammad Faizan <muhammadfaizan45890@gmail.com>
2026-05-22 15:37:05 +0500

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,23 +2,20 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-
     const mongoURI = process.env.MONGODB_URI;
 
     if (!mongoURI) {
-      throw new Error("MONGODB_URI is missing in .env");
+      throw new Error("MONGODB_URI is missing in environment variables");
     }
 
     await mongoose.connect(mongoURI);
 
     console.log("✅ MongoDB connected successfully");
-
   } catch (error) {
-
-    console.log("❌ MongoDB connection error");
+    console.log("❌ MongoDB connection error:");
     console.log(error);
 
-    process.exit(1);
+    throw error; // IMPORTANT for Vercel
   }
 };
 

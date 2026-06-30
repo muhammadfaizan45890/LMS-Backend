1edf9ef5551483f290ed2476399fc18e66d91bb0
Muhammad Faizan <muhammadfaizan45890@gmail.com>
2026-06-30 22:23:52 +0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -2,55 +2,55 @@
 
 
 
-// import passport from "passport";
-// import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-// import { User } from "../models/userModel.js";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { User } from "../models/userModel.js";
 
-// // Use environment variable for callback URL (fallback for local development)
-// const callbackURL = process.env.GOOGLE_CALLBACK_URL || "http://localhost:8000/auth/google/callback";
+// Use environment variable for callback URL (fallback for local development)
+const callbackURL = process.env.GOOGLE_CALLBACK_URL || "http://localhost:8000/auth/google/callback";
 
-// passport.use(
-//   new GoogleStrategy(
-//     {
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       callbackURL,   // 👈 now dynamic
-//     },
-//     async (accessToken, refreshToken, profile, cb) => {
-//       try {
-//         const email = profile.emails[0].value;
-//         const googleId = profile.id;
-//         const username = profile.displayName;
-//         const avatar = profile.photos[0]?.value;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL,   // 👈 now dynamic
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
+        const email = profile.emails[0].value;
+        const googleId = profile.id;
+        const username = profile.displayName;
+        const avatar = profile.photos[0]?.value;
 
-//         let user = await User.findOne({ email });
+        let user = await User.findOne({ email });
 
-//         if (user) {
-//           if (!user.googleId) {
-//             user.googleId = googleId;
-//             user.avatar = avatar || user.avatar;
-//             user.isVerified = true;
-//             if (!user.username) user.username = username;
-//             await user.save();
-//           }
-//           user.isLoggedIn = true;
-//           await user.save();
-//           return cb(null, user);
-//         } else {
-//           user = await User.create({
-//             googleId,
-//             email,
-//             username,
-//             avatar,
-//             isLoggedIn: true,
-//             isVerified: true,
-//           });
-//           return cb(null, user);
-//         }
-//       } catch (error) {
-//         console.error("Google Auth Error:", error);
-//         return cb(error, null);
-//       }
-//     }
-//   )
-// );
+        if (user) {
+          if (!user.googleId) {
+            user.googleId = googleId;
+            user.avatar = avatar || user.avatar;
+            user.isVerified = true;
+            if (!user.username) user.username = username;
+            await user.save();
+          }
+          user.isLoggedIn = true;
+          await user.save();
+          return cb(null, user);
+        } else {
+          user = await User.create({
+            googleId,
+            email,
+            username,
+            avatar,
+            isLoggedIn: true,
+            isVerified: true,
+          });
+          return cb(null, user);
+        }
+      } catch (error) {
+        console.error("Google Auth Error:", error);
+        return cb(error, null);
+      }
+    }
+  )
+);
